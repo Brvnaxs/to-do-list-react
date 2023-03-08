18f2c563dfcd16f74141ea33f62cc61abd454773
Aluno <aluno@naverecife.local>
2023-03-08 20:57:18 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useState} from 'react'
+import './style.css'
+function App(){
+  const [titulo, setTitulo] = useState()
+  const [descricao, setDescricao] = useState()
+  //const [data, setData] = useState()
+  const [lista, setLista] = useState([])
+  
+  function HandleAddItem(){
+    const item = {'titulo': titulo, 'descricao': descricao, 'data': new Date().toISOString()}
+    setLista([item, ...lista]) 
+  }
+  return(
+    <div>
+      <input value={titulo} onChange={e => setTitulo(e.target.value)} placeholder='Título'></input>
+      <input value={descricao} onChange={e => setDescricao(e.target.value)} placeholder='Descrição'></input>
+      <button onClick={HandleAddItem}>Adicionar</button>
+      <ul>
+        {lista.map(t => (
+          <li><div>
+          <h1>{t.titulo}</h1>
+          <p>{t.descricao}</p>
+          <p>{t.data}</p>
+      </div></li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
-
 export default App;
